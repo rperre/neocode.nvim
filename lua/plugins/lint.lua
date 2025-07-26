4302ca02b6624fb1e40b9ec194dcfe4644cd209d
Ricardo Perre <rperre@Ricardos-MacBook-Air.local>
2025-07-26 21:38:39 +0100

    Added nvim-dap

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,4 +1,13 @@
 return {
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'suketa/nvim-dap-ruby',
+    },
+    config = function()
+      require('dap-ruby').setup()
+    end,
+  },
 
   { -- Linting
     'mfussenegger/nvim-lint',
